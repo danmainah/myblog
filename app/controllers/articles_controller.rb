f8971c9202a1f525b16137eec9f3b22c610518db
unknown <danielmainawaweru@gmail.com>
2021-06-14 17:57:29 +0300

    Config for deployment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,11 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new(article_params)
   end
 
   def new
